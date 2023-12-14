752c13a4c3ce19a62429d837757c3e9d6d940566
Rayane MOULA <95938506+SlevinKl@users.noreply.github.com>
2023-12-14 12:18:20 +0100

    initialize project and delete not necessary documents

--- src/main/java/com/chatop/ChaTopApplication.java
+++ src/main/java/com/chatop/ChaTopApplication.java
@@ -1,4 +1,4 @@
-package com.chatop.ChaTop;
+package com.chatop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class ChaTopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChaTopApplication.class, args);
 	}
-
 }
