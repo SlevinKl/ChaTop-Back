8f576cb264fad08b4629342851492cdae5a06800
Rayane MOULA <95938506+SlevinKl@users.noreply.github.com>
2023-12-14 12:44:23 +0100

    add exception

---
+++ src/main/java/com/chatop/exception/InternalServerErrorException.java
@@ -0,0 +1,18 @@
+package com.chatop.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalServerErrorException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+	public InternalServerErrorException() {
+        super("Internal Server Error");
+    }
+
+    public InternalServerErrorException(String message) {
+        super(message);
+    }
+}
